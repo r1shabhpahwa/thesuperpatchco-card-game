4bdb4635c9a415c8a4fd7fff1033af86ed1c354f
Rishabh Pahwa <rishabhpahwa@gmail.com>
2025-02-13 19:15:59 -0500

    fixes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -40,7 +40,7 @@ function App() {
 
     try {
       const res = await axios.post(
-        "https://172.21.0.2:3200/cards",
+        "https://sp-backend.p1xelhub.xyz/cards",
         { cards: JSON.parse(cards) },
         { headers: { "Content-Type": "application/json" } }
       );
@@ -59,7 +59,7 @@ function App() {
 
   const fetchHistory = async () => {
     try {
-      const res = await axios.get("https://172.21.0.2:3200/history");
+      const res = await axios.get("https://sp-backend.p1xelhub.xyz/history");
       setHistory(res.data.history);
     } catch (err) {
       setError("Failed to fetch history");
